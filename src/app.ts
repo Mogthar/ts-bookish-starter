2f190c08bbabcb4c90c1de083787f12a425adb49
Amelia Blackwell <Amelia.Blackwell@softwire.com>
2023-07-07 17:20:28 +0100

    getallbooks endpoint implemented

--- src/app.ts
+++ src/app.ts
@@ -24,50 +24,16 @@ var config = {
 };
 
 var connection = new Connection(config);
-
 connection.on('connect', function(err){
-    if(err)
-    {
+    if(err){
         console.log(err);
     }
     console.log('Success');
-    getAllBooks();
 });
 
 connection.connect();
 
-function getAllBooks()
-{
-    var request = new Request('SELECT * FROM CATALOGUE;', (err, rowcount, rows) => {
-        if(err)
-        {
-            throw err;
-        }                                                    
-    });
-    let bookArray = [];
-    request.on('row', function(columns){
-        let data = [];
-        columns.forEach(function(column) {
-            data.push(column.value);
-        });
-        bookArray.push(new Book(data[0], data[1], data[2]));
-    });
 
-    request.on('requestCompleted', function(rowCount, more){
-        console.log(bookArray);
-        connection.close();
-    });
-
-    connection.execSql(request);
-
-}
-
-
-
-
-
-
-/*
 const port = process.env['PORT'] || 3000;
 
 const app = express();
@@ -75,17 +41,41 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-*/
 
 /**
  * Primary app routes.
  */
 
-/*
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/books', bookRoutes);
+app.get('/getallbooks', async function (req, res) {
+    let bookArray = await getAllBooks()
+    res.send(bookArray)
+    });
 
-*/
+function getAllBooks() {
+    
+    return new Promise((resolve, reject) => {
+        var request = new Request('SELECT * FROM CATALOGUE;', (err) => {
+            if(err){
+                throw err;
+            }                                                    
+        });
+        let bookArray = [];
+        request.on('row', function(columns){
+            let data = [];
+            columns.forEach(function(column) {
+                data.push(column.value);
+            });
+            bookArray.push(new Book(data[0], data[1], data[2]));
+        });
+        request.on('requestCompleted', function(rowCount, more){
+            resolve(bookArray)
+        });
+        connection.execSql(request);
+    })
+}
+
 class Book{
     isbn: number;
     bookName: string;
